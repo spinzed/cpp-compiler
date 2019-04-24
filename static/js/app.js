e1c124b249058a0d199657a27d3f8d2fee09e20b
Spinzed <davor.najev@gmail.com>
2019-04-24 16:10:16 +0200

    Added instructions to app.js for setting up the editor

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,15 @@
 const ed = new Editor("editor");
+
+// To activate editors's features, you just need to make an instance of the
+// editor class. The only attribute is the ID of the core editor div.
+// Structure of the editor+s elements in html:
+// 
+// div #EDTR-NAME
+// └───div #EDTR-NAME_scroll
+//     ├───div #EDTR-NAME_inner
+//     ├───div #EDTR-NAME_rowline
+//     └───div #EDTR-NAME_core
+//         ├───div #EDTR-NAME_editor_input
+//         └───div #EDTR-NAME_editor_rows
+// 
+// EDTR-NAME - ID of the core editor div
