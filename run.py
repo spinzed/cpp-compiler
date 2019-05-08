2b25e4eb031eda23ea0f209e95c2e6eea280ada8
isakal <sakyYT@gmail.com>
2019-05-08 23:11:26 +0200

    Trying to work API

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
-        print(request.get_json()["kod"])
-    else:
-        pass
+        code = request.get_json()["kod"]
+        print(code)
+    elif request.method == "GET":
+        return "saki"
     return render_template("index.html", req=request)
 
 if __name__ == "__main__":
